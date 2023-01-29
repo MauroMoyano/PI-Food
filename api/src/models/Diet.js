c5b54494352f86c77c184a77ef1c0a09807dad66
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-01-28 22:32:06 -0300

    trabajando en bdd y rutas

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -11,24 +11,7 @@ module.exports = (sequelize) => {
     },
     name:{
       type: DataTypes.STRING(100),
-      unique: true,
       allowNull: false
-    },
-    summary:{
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    healthScore:{
-      type: DataTypes.FLOAT
-    },
-    score:{
-      type: DataTypes.FLOAT
-    },
-    image: {
-      type: DataTypes.STRING
-    },
-    steps: {
-      type: DataTypes.TEXT
-    },
+    }
   });
 };
