290c7215e611e9dd223ffeb9fe72485991df9b0a
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-02-23 02:28:00 -0300

    "deploy 1"

--- api/src/routes/Controllers/getHome.js
+++ api/src/routes/Controllers/getHome.js
@@ -4,16 +4,14 @@ const {API_PSW, URLSPOO} = process.env
 
 
 const getHome = async () => {
-    // console.log("esta entrando a la funcion controller getHome")
+    console.log("esta entrando a la funcion controller getHome")
     let aux = await Recipe.findAll()
-    // console.log("BDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
     let {data} = await axios.get(`${URLSPOO}?${API_PSW}&addRecipeInformation=true&number=20`)
     const {results} = data
 
     let obj = [];
     if (results.length) {
         obj = results.map((element) => {
-            // console.log("esta entrando a la funcion element", element)
             return {
                 id: element.id,
                 title: element.title,
@@ -24,7 +22,6 @@ const getHome = async () => {
             }
         })
     }
-    console.log("apiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", obj[0])
     return [...aux, ...obj]
 
 }
